89dfef60c3d4932dd13011a1172a3edfdba74066
shahebaz <shahebaz.khan444@gmail.com>
2021-11-08 13:04:55 +0530

    server changed

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import {ApolloClient,InMemoryCache, createHttpLink, ApolloProvider} from "@apoll
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://still-retreat-18921.herokuapp.com/'
 })
 
 const authLink = setContext(() =>{
